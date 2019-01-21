3aebe7fa862391889b3f63773a622d25774784e6
xen0bit <xenobitbugs@outlook.com>
2019-01-21 18:00:17 -0500

    cleanup

--- shell/setip.sh
+++ shell/setip.sh
@@ -1,11 +1,6 @@
 echo "Setting IPTables rules for packet capture..."
-#OSRS
-#C-->S Port 54258
+#Client Port 54258-->Server
 #sudo iptables -t raw -A PREROUTING -p tcp --source-port 54258 -j NFQUEUE --queue-num 1
-#S-->C Port 43594
+#Server Port 80-->Client
 sudo iptables -t raw -A PREROUTING -p tcp --source-port 80 -j NFQUEUE --queue-num 1
-#iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
-#sudo iptables -t raw -A PREROUTING -p tcp --source-port 1000:55000 -j NFQUEUE --queue-num 1
-#sudo iptables -t raw -A PREROUTING -p tcp --source-port 80 -j NFQUEUE --queue-num 1
-#sudo iptables -t raw -A PREROUTING -p tcp --destination-port 43595 -j NFQUEUE --queue-num 1
 
